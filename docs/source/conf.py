f9a09b60315aaa594ad85705e7d56e7a8e76cece
Nicolas Bock <nicolasbock@gmail.com>
2025-07-10 14:34:24 +0200

    Fix display of version
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,10 +11,19 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+def git_tag():
+    """Return the current git tag."""
+    import subprocess
+    try:
+        return subprocess.check_output(['git', 'describe', '--tags', '--always']).strip().decode('utf-8')
+    except subprocess.CalledProcessError:
+        return 'unknown'
+
 project = 'juju-lnav'
 copyright = '2025, Nicolas Bock <nicolas.bock@canonical.com>'
 author = 'Nicolas Bock <nicolas.bock@canonical.com>'
-release = '0.1'
+release = git_tag()
+version = release  # Use the same version for both release and version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -36,7 +45,7 @@ source_suffix = {
 }
 
 # Set the master document to readme.md instead of index.rst
-# master_doc = 'index.md'
+master_doc = 'index'
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -48,3 +57,25 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# Alabaster theme options
+html_theme_options = {
+    'show_powered_by': False,
+    'github_user': 'nicolasbock',
+    'github_repo': 'juju-lnav',
+    'github_banner': True,
+    'show_related': False,
+    'note_bg': '#FFF59C',
+    'sidebar_width': '220px',
+    'page_width': '1000px',
+    'show_relbars': True,
+    'description': f'Version {release}',
+    'logo_name': True,
+    'logo_text_align': 'center',
+}
+
+# Add version information to the HTML context
+html_context = {
+    'display_version': True,
+    'current_version': release,
+}
